2ebcab56a1897862288026f04097aaabfd334cc9
Amanda R <142240337+arajda@users.noreply.github.com>
2023-11-26 15:59:34 -0500

    Imports of necessary packages for software
    Imports of packages such as pandas, sklearning, numpy, etc. to help organize and label the scraped data. This will organize the data to ensure software is accurate when giving out reccomendations.

---
+++ Testing_1.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+from sklearn.preprocessing import LabelEncoder
+from textblob import TextBlob
+from sklearn.impute import KNNImputer
+
+from numpy.linalg import norm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.compose import ColumnTransformer
+
