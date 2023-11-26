2503023a8d817d42f93143cc6e1efede3bd8d67b
Amanda R <142240337+arajda@users.noreply.github.com>
2023-11-26 16:16:09 -0500

    Data Sorting
    Data set uploaded, won't read or sort properly yet.

---
+++ DataSort.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+from sklearn.preprocessing import LabelEncoder
+from textblob import TextBlob
+from sklearn.impute import KNNImputer
+
+from numpy.linalg import norm
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.compose import ColumnTransformer
+
+df = pd.read_csv("/Users/amandarajda/Perfume Pocket/Perfumes_Recommender/Datasets/final_df.csv")
+df.info()
+df.shape
+df.head()
